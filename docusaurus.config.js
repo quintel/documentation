009f4211061c31d6774a143fde24a7ece4c2b68f
Anthony Williams <hi@antw.dev>
2020-04-26 15:07:01 +0100

    Adjust primary color and site logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   tagline: 'ETM documentation',
   url: 'https://docs.energytransitionmodel.com',
   baseUrl: '/',
-  favicon: 'img/favicon.svg',
+  favicon: 'img/logo.svg',
   organizationName: 'quintel', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
   themeConfig: {
