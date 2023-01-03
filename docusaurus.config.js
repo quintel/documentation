b1ee6a267030a4e08f7f4b4c3936e46b700b29a9
Anthony Williams <hi@antw.dev>
2023-01-03 09:25:49 +0000

    Disable contextual search for now
    This is causing Algolia to return no results

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,7 @@ module.exports = {
       appId: "NQGE6UZW7V",
       apiKey: "5dcd7183ae0e2678ac84df798c9bafc0",
       indexName: "energytransitionmodel",
+      contextualSearch: false,
     },
   },
   presets: [
