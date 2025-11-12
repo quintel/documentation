51d50c4a3349ec4d6b29c880698c8cac9db745a1
aaccensi <andreu.accensi@gmail.com>
2025-11-12 11:49:09 +0100

    Updating API for collections
    References #265

--- data/api/collections.js
+++ data/api/collections.js
@@ -8,18 +8,18 @@ export default {
   index: {
     endpoint: "/api/v3/collections",
     method: "GET",
-    path_parameters: [
-      {
-        name: "page",
-        type: "number",
-        description: "the page number to fetch",
-      },
-      {
-        name: "limit",
-        type: "number",
-        description: "the number of items per page",
-      },
-    ],
+    // path_parameters: [ // These don't work anymore even though we get "data" and "links" in the JSON
+    //   {
+    //     name: "page",
+    //     type: "number",
+    //     description: "the page number to fetch",
+    //   },
+    //   {
+    //     name: "limit",
+    //     type: "number",
+    //     description: "the number of items per page",
+    //   },
+    // ],
     token: { scopes: ["scenarios:read"] },
   },
   show: {
@@ -49,9 +49,16 @@ export default {
         name: "saved_scenario_ids",
         type: "[]number",
         description:
-          "the IDs of the underlying saved scenarios; at least one is required between scenarios and saved_scenarios",
+          "the IDs of the underlying saved scenarios (required); at least one is required between scenarios and saved_scenarios",
         required: true,
       },
+      // {
+      //   name: "interpolation", // I think we need to add this but as of now it does nothing
+      //   type: "boolean",
+      //   description:
+      //     "true for a transition path (true by default); ",
+      //   required: false,
+      // },
     ],
     token: { scopes: ["scenarios:read", "scenarios:write"] },
   },
@@ -61,29 +68,27 @@ export default {
     path_parameters: [idParam],
     parameters: [
       {
-        name: "scenario_id",
-        type: "integer",
-        description: "the ID of the underlying scenario (required)",
-      },
-      {
         name: "title",
         type: "string",
-        description: "what to call the saved scenario (required)",
+        description: "what to call the saved scenario",
       },
       {
-        name: "description",
-        type: "string",
-        description: "an optional description for the saved scenario",
+        name: "scenario_ids",
+        type: "[]number",
+        description:
+          "the IDs of the underlying scenarios",
+        required: false,
       },
       {
-        name: "private",
-        type: "boolean",
-        description: "whether the scenario can be viewed only by the owner",
+        name: "saved_scenario_ids",
+        type: "[]number",
+        description:
+          "the IDs of the underlying saved scenarios",
       },
       {
         name: "discarded",
         type: "boolean",
-        description: "whether the scenario should be in the owner's trash",
+        description: "whether the collection should be in the owner's trash",
       },
     ],
     token: { scopes: ["scenarios:read", "scenarios:write"] },
