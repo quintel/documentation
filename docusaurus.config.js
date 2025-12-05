e01c801c3b3b4e6333fbd74187e32f7ee2b2b55d
louispt1 <louisparkestalbot1@gmail.com>
2025-12-05 11:46:03 +0100

    Link to the ETM by clicking on the Energy Transition Model in the top left

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,8 @@ module.exports = {
       logo: {
         alt: "Logo",
         src: "img/logo.svg",
+        href: "https://energytransitionmodel.com/",
+        target: "_blank",
       },
     },
     footer: {
