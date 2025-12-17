4a711f71a4867b14b4900d5e10f28d26683c0f6a
aaccensi <andreu.accensi@gmail.com>
2025-12-17 11:26:46 +0100

    Add documentation pertinent to the scenario interpolate API endpoint
    References quintel/etengine#1684

--- data/api/scenario.js
+++ data/api/scenario.js
@@ -33,6 +33,32 @@ export default {
     method: "POST",
     token: { scopes: ["scenarios:read"], type: "optional" },
   },
+  interpolate: {
+    endpoint: "/api/v3/scenarios/{id}/interpolate",
+    method: "POST",
+    path_parameters: [scenarioIdParam],
+    parameters: [
+      {
+        name: "start_scenario_id",
+        type: "number",
+        description:
+          "the ID of the scenario whose input values are used as the interpolation starting point, if provided. Must match the endpoint scenario’s start year and area code",
+        required: false,
+      },
+      {
+        name: "end_year",
+        type: "number",
+        description:
+          `end year of the scenario (interpolation target year)<br>
+          <ul>
+            <li>Must fall between the endpoint scenario’s start and end years when <code>start_scenario_id</code> is <strong>not</strong> provided</li>
+            <li>Must fall between the start scenario and the endpoint scenario end years when <code>start_scenario_id</code> is provided</li>
+          </ul>`,
+        required: true,
+      },
+    ],
+    token: { scopes: ["scenarios:read"], type: "optional" },
+  },
   update: {
     endpoint: "/api/v3/scenarios/{id}",
     method: "PUT",
